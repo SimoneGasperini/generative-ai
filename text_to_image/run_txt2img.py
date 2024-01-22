9034fe59532e3307b9c77c2c4389eaede44a3dba
SimoneGasperini <simone.gasperini4@unibo.it>
2024-01-22 12:35:07 +0100

    Add text to image generation script

---
+++ text_to_image/run_txt2img.py
@@ -0,0 +1,16 @@
+import os
+import replicate
+
+
+prompt = "An oil painting of a humanoid robot playing chess"
+
+
+with open("../replicate_api_token.txt") as file:
+    os.environ["REPLICATE_API_TOKEN"] = file.read(40)
+
+out_url = replicate.run(
+    ref="stability-ai/stable-diffusion:27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478",
+    input={"prompt": prompt}
+)[0]
+
+os.system(command=f'wget {out_url}')
