9758c8ab93ed73398a1ef29dee9d9ca31d29861e
SimoneGasperini <simone.gasperini4@unibo.it>
2024-01-22 11:07:26 +0100

    Minor fix

--- image_style_transfer/run_img_st.py
+++ image_style_transfer/run_img_st.py
@@ -8,7 +8,7 @@ style_image = open(f"{directory}/notte_stellata.png", mode="rb")
 
 
 with open("../replicate_api_token.txt") as file:
-    os.environ["REPLICATE_API_TOKEN"] = file.read()
+    os.environ["REPLICATE_API_TOKEN"] = file.read(40)
 
 out_url = replicate.run(
     ref="huage001/adaattn:957250892e7125f4834c5b5e5b5b2b43dc18ff174a6d70958574d08298567a21",
